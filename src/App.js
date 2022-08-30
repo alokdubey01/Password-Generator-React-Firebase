b13643704fed7eea336e12953ca73dc433661cd8
Alok Dubey <alok73327@gmail.com>
2022-08-30 22:50:10 +0530

    loading animation

--- src/App.js
+++ src/App.js
@@ -6,28 +6,44 @@ import { Login } from "./components/Login";
 import firebase from "./firebase";
 import { Header } from "./components/Header";
 
-
 function App() {
   const [user, setUser] = React.useState(null);
+  const [loading, setLoading] = React.useState(false);
 
   React.useEffect(() => {
+    setLoading(true);
     firebase.auth().onAuthStateChanged((user) => {
       setUser(user);
       localStorage.setItem("user", user.uid);
+      setLoading(false);
     });
   }, []);
   return (
     <div className="App">
-      <Router>
-        {user ? <Header alt={user.displayName} src={user.photoURL} /> : ""}
-        <Routes>
-          {user ? (
-            <Route exact path="/" element={<Main user={user} />} />
-          ) : (
-            <Route exact path="/" element={<Login />} />
-          )}
-        </Routes>
-      </Router>
+      {loading ? (
+        <div style={{ background: "#222", height: "100vh", width: "100wh" }}>
+          <div id="load">
+            <div>G</div>
+            <div>N</div>
+            <div>I</div>
+            <div>D</div>
+            <div>A</div>
+            <div>O</div>
+            <div>L</div>
+          </div>
+        </div>
+      ) : (
+        <Router>
+          {user ? <Header alt={user.displayName} src={user.photoURL} /> : ""}
+          <Routes>
+            {user ? (
+              <Route exact path="/" element={<Main user={user} />} />
+            ) : (
+              <Route exact path="/" element={<Login />} />
+            )}
+          </Routes>
+        </Router>
+      )}
     </div>
   );
 }
